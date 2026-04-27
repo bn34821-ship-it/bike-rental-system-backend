dcc468d01fc169613ed016cbe96dc0b285ae70fb
bn34821-ship-it <bn34821@gmail.com>
2026-04-27 07:47:47 +0530

    Delete src directory

--- src/routes/skippedDays.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as skippedDaysController from "../controllers/skippedDaysController.js";
-
-const router = Router();
-
-router.get("/", skippedDaysController.getSkippedDays);
-router.post("/", skippedDaysController.addSkippedDay);
-
-export default router;
