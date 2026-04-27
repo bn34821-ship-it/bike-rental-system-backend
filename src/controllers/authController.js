35987a36c5946bcedd47c23397dfc3edb7afa27f
bn34821-ship-it <bn34821@gmail.com>
2026-04-27 08:15:41 +0530

    Delete src directory

--- src/controllers/authController.js
+++
@@ -1,7 +0,0 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-import * as authService from "../services/authService.js";
-
-export const login = asyncHandler(async (req, res) => {
-  const result = await authService.loginWithPhone(req.body);
-  res.json({ success: true, data: result });
-});
--- src/routes/trackingRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getRouteHistory, getTracking } from "../controllers/trackingController.js";
-
-const router = Router();
-
-router.get("/tracking/:id", getTracking);
-router.get("/routes/:id", getRouteHistory);
-
-export default router;
