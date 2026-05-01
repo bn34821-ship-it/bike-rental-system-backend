7e0c76e75e1103bddb98a919dc853a0a5517d8d7
bn34821-ship-it <bn34821@gmail.com>
2026-05-01 12:54:40 +0530

    Delete src directory

--- src/realtime/events.js
+++
@@ -1,7 +0,0 @@
-/** Future WebSocket / Supabase Realtime event names */
-export const RealtimeEvents = {
-  BIKE_STATUS: "bike:status",
-  ORDER_UPDATE: "order:update",
-  RENTAL_UPDATE: "rental:update",
-  PARTNER_STATUS: "partner:status",
-};
