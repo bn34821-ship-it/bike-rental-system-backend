af5f28dfd0bd539ceb2b65b7c50a37f4c50d1bdd
bn34821-ship-it <bn34821@gmail.com>
2026-05-01 12:56:00 +0530

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-import supabase from "./src/utils/supabaseClient.js";
-
-async function run() {
-  const { data, error } = await supabase.from("payment_configs").select("*").limit(1);
-  console.log("TEST_DB_RESULT:", JSON.stringify({ data, error }));
-}
-run();
