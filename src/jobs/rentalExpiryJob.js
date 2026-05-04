5b1ea72ad9652571ae5dbdd52f886c1d1ce2acad
bn34821-ship-it <bn34821@gmail.com>
2026-05-04 14:07:24 +0530

    Delete src directory

--- src/jobs/rentalExpiryJob.js
+++
@@ -1,9 +0,0 @@
-import * as rentalService from "../services/rentalService.js";
-
-export async function runRentalExpirySweep() {
-  const results = await rentalService.expireRentalsPastEnd();
-  if (results.length) {
-    console.log(`[jobs] Auto-ended ${results.length} rental(s)`);
-  }
-  return results;
-}
